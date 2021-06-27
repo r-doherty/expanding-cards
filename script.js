cd013698dc71ad8e5b777c26738a9181c8e907ad
unknown <rjd.atx@gmail.com>
2021-06-27 14:37:50 -0500

    javascript interactions and final media/images

---
+++ script.js
@@ -0,0 +1,15 @@
+// Node list of cards object
+const panels = document.querySelectorAll(`.panel`);
+
+panels.forEach((panel) => {
+	panel.addEventListener(`click`, () => {
+		removeActiveClasses();
+		panel.classList.add(`active`);
+	});
+});
+
+function removeActiveClasses() {
+	panels.forEach((panel) => {
+		panel.classList.remove(`active`);
+	});
+}
